ae1a732e0b5780e3bcfd9e961fce91af2149ae05
davefalkus <falkus@hotmail.com>
2017-05-09 09:30:52 -0700

    User policy report update

--- UserPolicyReport/User_Policy_Report_Get.ps1
+++ UserPolicyReport/User_Policy_Report_Get.ps1
@@ -177,18 +177,18 @@ param
 # Defining Variables
 $graphApiVersion = "v1.0"
 $User_resource = "users"
-
+    
     try {
-
+        
         if($userPrincipalName -eq "" -or $userPrincipalName -eq $null){
-
+        
         $uri = "https://graph.microsoft.com/$graphApiVersion/$($User_resource)"
         (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
-
+        
         }
 
         else {
-
+            
             if($Property -eq "" -or $Property -eq $null){
 
             $uri = "https://graph.microsoft.com/$graphApiVersion/$($User_resource)/$userPrincipalName"
@@ -206,7 +206,7 @@ $User_resource = "users"
             }
 
         }
-
+    
     }
 
     catch {
@@ -253,9 +253,9 @@ param
 # Defining Variables
 $graphApiVersion = "beta"
 $Resource = "users/$UserID/managedDevices"
-
+    
     try {
-
+    
     $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
     Write-Verbose $uri
     (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
@@ -307,7 +307,7 @@ param
 # Defining Variables
 $graphApiVersion = "v1.0"
 $Group_resource = "groups"
-
+    
     try {
 
         if($id){
@@ -316,28 +316,28 @@ $Group_resource = "groups"
         (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
 
         }
-
+        
         elseif($GroupName -eq "" -or $GroupName -eq $null){
-
+        
         $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)"
         (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
-
+        
         }
 
         else {
-
+            
             if(!$Members){
 
             $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=displayname eq '$GroupName'"
             (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
-
+            
             }
-
+            
             elseif($Members){
-
+            
             $uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=displayname eq '$GroupName'"
             $Group = (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
-
+            
                 if($Group){
 
                 $GID = $Group.id
@@ -351,7 +351,7 @@ $Group_resource = "groups"
                 }
 
             }
-
+        
         }
 
     }
@@ -399,67 +399,62 @@ NAME: Get-DeviceCompliancePolicy
 
 param
 (
-    $Name,
     [switch]$Android,
     [switch]$iOS,
-    [switch]$Win10
+    [switch]$Win10,
+    $Name
 )
 
 $graphApiVersion = "Beta"
-$Resource = "deviceManagement/deviceCompliancePolicies"
-
+$DCP_resource = "deviceManagement/deviceCompliancePolicies"
+    
     try {
+        
+        # windows81CompliancePolicy
+        # windowsPhone81CompliancePolicy
 
         $Count_Params = 0
 
         if($Android.IsPresent){ $Count_Params++ }
         if($iOS.IsPresent){ $Count_Params++ }
         if($Win10.IsPresent){ $Count_Params++ }
-        if($Name.IsPresent){ $Count_Params++ }
 
         if($Count_Params -gt 1){
-
+        
         write-host "Multiple parameters set, specify a single parameter -Android -iOS or -Win10 against the function" -f Red
-
+        
         }
-
+        
         elseif($Android){
-
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
         (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value | Where-Object { ($_.'@odata.type').contains("android") }
-
+        
         }
-
+        
         elseif($iOS){
-
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
         (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value | Where-Object { ($_.'@odata.type').contains("ios") }
-
+        
         }
 
         elseif($Win10){
-
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
         (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value | Where-Object { ($_.'@odata.type').contains("windows10CompliancePolicy") }
-
+        
         }
-
-        elseif($Name){
-
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
-        (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value | Where-Object { ($_.'displayName').contains("$Name") }
-
-        }
-
+        
         else {
 
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
         (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
 
         }
 
     }
-
+    
     catch {
 
     $ex = $_.Exception
@@ -487,7 +482,7 @@ This function is used to get device compliance policy assignment from the Graph 
 .DESCRIPTION
 The function connects to the Graph API Interface and gets a device compliance policy assignment
 .EXAMPLE
-Get-DeviceCompliancePolicyAssignment -id guid
+Get-DeviceCompliancePolicyAssignment $id guid
 Returns any device compliance policy assignment configured in Intune
 .NOTES
 NAME: Get-DeviceCompliancePolicyAssignment
@@ -503,14 +498,14 @@ param
 
 $graphApiVersion = "Beta"
 $DCP_resource = "deviceManagement/deviceCompliancePolicies"
-
+    
     try {
 
     $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)/$id/groupAssignments"
     (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
-
+        
     }
-
+    
     catch {
 
     $ex = $_.Exception
@@ -530,111 +525,145 @@ $DCP_resource = "deviceManagement/deviceCompliancePolicies"
 
 ####################################################
 
-Function Get-DeviceConfigurationPolicy(){
-
-<#
-.SYNOPSIS
-This function is used to get device configuration policies from the Graph API REST interface
-.DESCRIPTION
-The function connects to the Graph API Interface and gets any device configuration policies
-.EXAMPLE
-Get-DeviceConfigurationPolicy
-Returns any device configuration policies configured in Intune
-.NOTES
-NAME: Get-DeviceConfigurationPolicy
-#>
+Function Get-UserDeviceStatus(){
 
 [cmdletbinding()]
 
 param
 (
-    $name
+    [switch]$Analyze
 )
 
-$graphApiVersion = "Beta"
-$DCP_resource = "deviceManagement/deviceConfigurations"
+Write-Host "Getting User Devices..." -ForegroundColor Yellow
+Write-Host
 
-    try {
+$UserDevices = Get-AADUserDevices -UserID $UserID
 
-        if($Name){
+    if($UserDevices){
 
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
-        (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value | Where-Object { ($_.'displayName').contains("$Name") }
+        write-host "-------------------------------------------------------------------"
+        Write-Host
+
+        foreach($UserDevice in $UserDevices){
+
+        $UserDeviceId = $UserDevice.id
+        $UserDeviceName = $UserDevice.deviceName
+        $UserDeviceAADDeviceId = $UserDevice.azureActiveDirectoryDeviceId
+        $UserDeviceComplianceState = $UserDevice.complianceState
+
+        write-host "Device Name:" $UserDevice.deviceName -f Cyan
+        Write-Host "Device Id:" $UserDevice.id
+        write-host "Owner Type:" $UserDevice.ownerType
+        write-host "Last Sync Date:" $UserDevice.lastSyncDateTime
+        write-host "OS:" $UserDevice.operatingSystem
+        write-host "OS Version:" $UserDevice.osVersion
+
+            if($UserDevice.easActivated -eq $false){
+            write-host "EAS Activated:" $UserDevice.easActivated -ForegroundColor Red
+            }
+
+            else {
+            write-host "EAS Activated:" $UserDevice.easActivated
+            }
+
+        Write-Host "EAS DeviceId:" $UserDevice.easDeviceId
+
+            if($UserDevice.aadRegistered -eq $false){
+            write-host "AAD Registered:" $UserDevice.aadRegistered -ForegroundColor Red
+            }
+
+            else {
+            write-host "AAD Registered:" $UserDevice.aadRegistered
+            }
+        
+        write-host "Enrollment Type:" $UserDevice.enrollmentType
+        write-host "Management State:" $UserDevice.managementState
+
+            if($UserDevice.complianceState -eq "noncompliant"){
+            
+                write-host "Compliance State:" $UserDevice.complianceState -f Red
+
+                $uri = "https://graph.microsoft.com/beta/managedDevices/$UserDeviceId/deviceCompliancePolicyStates"
+                
+                $deviceCompliancePolicyStates = (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
+
+                    foreach($DCPS in $deviceCompliancePolicyStates){
+
+                        if($DCPS.State -ne "notApplicable"){
+
+                        Write-Host
+                        Write-Host "Non Compliant Policy for device $UserDeviceName" -ForegroundColor Yellow
+                        write-host "Display Name:" $DCPS.displayName
+
+                        $SettingStatesId = $DCPS.id.split("_")[2]
+
+                        $uri = "https://graph.microsoft.com/beta/managedDevices/$UserDeviceId/deviceCompliancePolicyStates/$SettingStatesId/settingStates"
+
+                        $SettingStates = (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
+
+                            foreach($SS in $SettingStates){
+
+                                if($SS.state -eq "nonCompliant"){
+
+                                    write-host
+                                    Write-Host "Setting:" $SS.setting
+                                    Write-Host "State:" $SS.state -ForegroundColor Red
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                # Getting AAD Device using azureActiveDirectoryDeviceId property
+                $uri = "https://graph.microsoft.com/v1.0/devices?`$filter=deviceId eq '$UserDeviceAADDeviceId'"
+                $AADDevice = (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
+
+                $AAD_Compliant = $AADDevice.isCompliant
+
+                # Checking if AAD Device and Intune ManagedDevice state are the same value
+
+                Write-Host
+                Write-Host "Compliance State - AAD and ManagedDevices" -ForegroundColor Yellow
+                Write-Host "AAD Compliance State:" $AAD_Compliant
+                Write-Host "Intune Managed Device State:" $UserDeviceComplianceState
+            
+            }
+            
+            else {
+
+                write-host "Compliance State:" $UserDevice.complianceState -f Green
+
+                # Getting AAD Device using azureActiveDirectoryDeviceId property
+                $uri = "https://graph.microsoft.com/v1.0/devices?`$filter=deviceId eq '$UserDeviceAADDeviceId'"
+                $AADDevice = (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
+
+                $AAD_Compliant = $AADDevice.isCompliant
+
+                # Checking if AAD Device and Intune ManagedDevice state are the same value
+
+                Write-Host
+                Write-Host "Compliance State - AAD and ManagedDevices" -ForegroundColor Yellow
+                Write-Host "AAD Compliance State:" $AAD_Compliant
+                Write-Host "Intune Managed Device State:" $UserDeviceComplianceState
+            
+            }
+
+        write-host
+        write-host "-------------------------------------------------------------------"
+        Write-Host
 
         }
 
-        else {
-
-        $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)"
-        (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
-
-        }
-
     }
 
-    catch {
+    else {
 
-    $ex = $_.Exception
-    $errorResponse = $ex.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($errorResponse)
-    $reader.BaseStream.Position = 0
-    $reader.DiscardBufferedData()
-    $responseBody = $reader.ReadToEnd();
-    Write-Host "Response content:`n$responseBody" -f Red
-    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+    #write-host "User Devices:" -f Yellow
+    write-host "User has no devices"
     write-host
-    break
-
-    }
-
-}
-
-####################################################
-
-Function Get-DeviceConfigurationPolicyAssignment(){
-
-<#
-.SYNOPSIS
-This function is used to get device configuration policy assignment from the Graph API REST interface
-.DESCRIPTION
-The function connects to the Graph API Interface and gets a device configuration policy assignment
-.EXAMPLE
-Get-DeviceConfigurationPolicyAssignment $id guid
-Returns any device configuration policy assignment configured in Intune
-.NOTES
-NAME: Get-DeviceConfigurationPolicyAssignment
-#>
-
-[cmdletbinding()]
-
-param
-(
-    [Parameter(Mandatory=$true,HelpMessage="Enter id (guid) for the Device Configuration Policy you want to check assignment")]
-    $id
-)
-
-$graphApiVersion = "Beta"
-$DCP_resource = "deviceManagement/deviceConfigurations"
-
-    try {
-
-    $uri = "https://graph.microsoft.com/$graphApiVersion/$($DCP_resource)/$id/groupAssignments"
-    (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value
-
-    }
-
-    catch {
-
-    $ex = $_.Exception
-    $errorResponse = $ex.Response.GetResponseStream()
-    $reader = New-Object System.IO.StreamReader($errorResponse)
-    $reader.BaseStream.Position = 0
-    $reader.DiscardBufferedData()
-    $responseBody = $reader.ReadToEnd();
-    Write-Host "Response content:`n$responseBody" -f Red
-    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
-    write-host
-    break
 
     }
 
@@ -660,7 +689,7 @@ if($global:authToken){
         write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
         write-host
 
-            # Defining User Principal Name if not present
+            # Defining Azure AD tenant name, this is the name of your Azure Active Directory (do not use the verified domain name)
 
             if($User -eq $null -or $User -eq ""){
 
@@ -694,7 +723,7 @@ $global:authToken = Get-AuthToken -User $User
 
 ####################################################
 
-write-host
+Write-Host
 write-host "User Principal Name:" -f Yellow
 $UPN = Read-Host
 
@@ -710,85 +739,16 @@ write-host
 
 ####################################################
 
-$UserDevices = Get-AADUserDevices -UserID $UserID
-
-    if($UserDevices){
-
-        write-host "User Devices:" -f Yellow
-
-        foreach($UserDevice in $UserDevices){
-
-        write-host "Device Name:" $UserDevice.deviceName -f Cyan
-        write-host "Owner Type:" $UserDevice.ownerType
-        write-host "Last Sync Date:" $UserDevice.lastSyncDateTime
-        write-host "OS:" $UserDevice.operatingSystem
-        write-host "OS Version:" $UserDevice.osVersion
-        write-host "EAS Activated:" $UserDevice.easActivated
-        write-host "AAD Registered:" $UserDevice.aadRegistered
-        write-host "Enrollment Type:" $UserDevice.enrollmentType
-        write-host "Management State:" $UserDevice.managementState
-
-            if($UserDevice.complianceState -ne "compliant"){
-            write-host "Compliance State:" $UserDevice.complianceState -f Red
-            }
-            else {
-            write-host "Compliance State:" $UserDevice.complianceState -f Green
-            }
-
-        write-host
-
-        }
-
-    }
-
-    else {
-
-    write-host "User Devices:" -f Yellow
-    write-host "User has no devices"
-    write-host
-
-    }
-
-####################################################
-
 $MemberOf = Get-AADUser -userPrincipalName $UPN -Property MemberOf
-
-$DirectoryRole = $MemberOf | ? { $_.'@odata.type' -eq "#microsoft.graph.directoryRole" }
-
-    if($DirectoryRole){
-
-    write-host "Directory Role:" -f Yellow
-    $DirectoryRole.displayName
-    write-host
-
-    }
-
-####################################################
-
-$AssignedLicenses = Get-AADUser -userPrincipalName $UPN -Property assignedLicenses
-
-    if($AssignedLicenses){
-
-    write-host "User Assigned Skus:" -f Yellow
-
-        foreach($license in $AssignedLicenses.skuId){
-        $license
-        }
-
-    Write-Host
-
-    }
-
-####################################################
 
 $AADGroups = $MemberOf | ? { $_.'@odata.type' -eq "#microsoft.graph.group" }
 
     if($AADGroups){
 
-    write-host "AAD Group Membership:" -f Yellow
-
+    write-host "User AAD Group Membership:" -f Yellow
+        
         foreach($AADGroup in $AADGroups){
-
+        
         (Get-AADGroup -id $AADGroup.id).displayName
 
         }
@@ -811,7 +771,7 @@ $CPs = Get-DeviceCompliancePolicy
 
 if($CPs){
 
-    write-host "Device Compliance Policies:" -f Yellow
+    write-host "Assigned Compliance Policies:" -f Yellow
     $CP_Names = @()
 
     foreach($CP in $CPs){
@@ -823,10 +783,10 @@ if($CPs){
         if($DCPA){
 
             foreach($Com_Group in $DCPA){
-
+            
                 if($AADGroups.id -contains $Com_Group.targetGroupId){
 
-                $CP_Names += $CP.displayName
+                $CP_Names += $CP.displayName + " - " + $CP.'@odata.type'
 
                 }
 
@@ -837,15 +797,15 @@ if($CPs){
     }
 
     if($CP_Names -ne $null){
-
+    
     $CP_Names
-
+    
     }
-
+    
     else {
-
+    
     write-host "No Device Compliance Policies Assigned"
-
+    
     }
 
 }
@@ -861,46 +821,6 @@ write-host
 
 ####################################################
 
-$DCPs = Get-DeviceConfigurationPolicy
+Get-UserDeviceStatus
 
-if($DCPs){
-
-    write-host "Device Configuration Policies:" -f Yellow
-    $DCP_Names = @()
-
-    foreach($DCP in $DCPs){
-
-    $id = $DCP.id
-
-    $DDCPA = Get-DeviceConfigurationPolicyAssignment -id $id
-
-        if($AADGroups.id -contains $DDCPA.targetGroupId){
-
-        $DCP_Names += $DCP.displayName
-
-        }
-
-    }
-
-    if($DCP_Names -ne $null){
-
-    $DCP_Names
-
-    }
-
-    else {
-
-    write-host "No Device Configuration Policies Assigned"
-
-    }
-
-}
-
-else {
-
-write-host "Device Configuration Policies:" -f Yellow
-write-host "No Device Configuration Policies Assigned"
-
-}
-
-write-host
+####################################################
