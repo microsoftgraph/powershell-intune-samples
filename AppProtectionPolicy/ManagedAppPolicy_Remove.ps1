43e56c8e9ac870a0436672c2a2fe80f888ceee0b
Peter Richards <petrich@microsoft.com>
2017-06-05 16:00:51 -0700

    Update route for App Protection Policies
    - Updated URLs from managedAppPolicies -> deviceAppManagement/managedAppPlicies

--- AppProtectionPolicy/ManagedAppPolicy_Remove.ps1
+++ AppProtectionPolicy/ManagedAppPolicy_Remove.ps1
@@ -171,7 +171,7 @@ param
 )
 
 $graphApiVersion = "Beta"
-$Resource = "managedAppPolicies"
+$Resource = "deviceAppManagement/managedAppPolicies"
 
     try {
 
@@ -232,7 +232,7 @@ param
 )
 
 $graphApiVersion = "Beta"
-$Resource = "managedAppPolicies"
+$Resource = "deviceAppManagement/managedAppPolicies"
 
     try {
 
