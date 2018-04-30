5b77a1dd3a77a397699036d0ef7787d56639b1a3
davefalkus <falkus@hotmail.com>
2018-04-30 08:43:39 +0100

    Update to Terms and Condition Export
    Moving function

--- TermsAndConditions/TermsAndConditions_export.ps1
+++ TermsAndConditions/TermsAndConditions_export.ps1
@@ -1,7 +1,7 @@
 <#
 .COPYRIGHT		
-		Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
-		See LICENSE in the project root for license information.
+Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+See LICENSE in the project root for license information.
 	
 #>
 ####################################################
@@ -207,6 +207,98 @@ $resource = "deviceManagement/termsAndConditions"
 
 ####################################################
 
+Function Export-JSONData(){
+	
+<#
+.SYNOPSIS
+This function is used to export JSON data returned from Graph
+.DESCRIPTION
+This function is used to export JSON data returned from Graph
+.EXAMPLE
+Export-JSONData -JSON $JSON
+Export the JSON inputted on the function
+.NOTES
+NAME: Export-JSONData
+#>
+        
+param (
+    
+    $JSON,
+    $ExportPath
+    
+)
+        
+    try
+    {
+        
+        if ($JSON -eq "" -or $JSON -eq $null)
+        {
+            
+            write-host "No JSON specified, please specify valid JSON..." -f Red
+            
+        }
+        
+        elseif (!$ExportPath)
+        {
+            
+            write-host "No export path parameter set, please provide a path to export the file" -f Red
+            
+        }
+        
+        elseif (!(Test-Path $ExportPath))
+        {
+            
+            write-host "$ExportPath doesn't exist, can't export JSON Data" -f Red
+            
+        }
+        
+        else
+        {
+            
+            $JSON1 = ConvertTo-Json $JSON
+            
+            $JSON_Convert = $JSON1 | ConvertFrom-Json
+            
+            $displayName = $JSON_Convert.displayName
+            
+            $Properties = ($JSON_Convert | Get-Member | ? { $_.MemberType -eq "NoteProperty" }).Name
+            
+            $displayName = $JSON_Convert.displayName
+            
+            $FileName_CSV = "$DisplayName" + "_" + $(get-date -f dd-MM-yyyy-H-mm-ss) + ".csv"
+            $FileName_JSON = "$DisplayName" + "_" + $(get-date -f dd-MM-yyyy-H-mm-ss) + ".json"
+            
+            $Object = New-Object System.Object
+            
+            foreach ($Property in $Properties)
+            {
+                
+                $Object | Add-Member -MemberType NoteProperty -Name $Property -Value $JSON_Convert.$Property
+                
+            }
+            
+            write-host "Export Path:" "$ExportPath"
+            
+            $Object | Export-Csv -LiteralPath "$ExportPath\$FileName_CSV" -Delimiter "," -NoTypeInformation -Append
+            $JSON1 | Set-Content -LiteralPath "$ExportPath\$FileName_JSON"
+            write-host "CSV created in $ExportPath\$FileName_CSV..." -f cyan
+            write-host "JSON created in $ExportPath\$FileName_JSON..." -f cyan
+            
+        }
+        
+    }
+    
+    catch
+    {
+        
+        $_.Exception
+        
+    }
+    
+}
+    
+####################################################
+
 #region Authentication
 
 write-host
@@ -256,98 +348,6 @@ $global:authToken = Get-AuthToken -User $User
 }
 
 #endregion
-
-####################################################
-
-Function Export-JSONData(){
-	
-<#
-.SYNOPSIS
-This function is used to export JSON data returned from Graph
-.DESCRIPTION
-This function is used to export JSON data returned from Graph
-.EXAMPLE
-Export-JSONData -JSON $JSON
-Export the JSON inputted on the function
-.NOTES
-NAME: Export-JSONData
-#>
-	
-	param (
-		
-		$JSON,
-		$ExportPath
-		
-	)
-	
-	try
-	{
-		
-		if ($JSON -eq "" -or $JSON -eq $null)
-		{
-			
-			write-host "No JSON specified, please specify valid JSON..." -f Red
-			
-		}
-		
-		elseif (!$ExportPath)
-		{
-			
-			write-host "No export path parameter set, please provide a path to export the file" -f Red
-			
-		}
-		
-		elseif (!(Test-Path $ExportPath))
-		{
-			
-			write-host "$ExportPath doesn't exist, can't export JSON Data" -f Red
-			
-		}
-		
-		else
-		{
-			
-			$JSON1 = ConvertTo-Json $JSON
-			
-			$JSON_Convert = $JSON1 | ConvertFrom-Json
-			
-			$displayName = $JSON_Convert.displayName
-			
-			$Properties = ($JSON_Convert | Get-Member | ? { $_.MemberType -eq "NoteProperty" }).Name
-			
-			$displayName = $JSON_Convert.displayName
-			
-			$FileName_CSV = "$DisplayName" + "_" + $(get-date -f dd-MM-yyyy-H-mm-ss) + ".csv"
-			$FileName_JSON = "$DisplayName" + "_" + $(get-date -f dd-MM-yyyy-H-mm-ss) + ".json"
-			
-			$Object = New-Object System.Object
-			
-			foreach ($Property in $Properties)
-			{
-				
-				$Object | Add-Member -MemberType NoteProperty -Name $Property -Value $JSON_Convert.$Property
-				
-			}
-			
-			write-host "Export Path:" "$ExportPath"
-			
-			$Object | Export-Csv -LiteralPath "$ExportPath\$FileName_CSV" -Delimiter "," -NoTypeInformation -Append
-			$JSON1 | Set-Content -LiteralPath "$ExportPath\$FileName_JSON"
-			write-host "CSV created in $ExportPath\$FileName_CSV..." -f cyan
-			write-host "JSON created in $ExportPath\$FileName_JSON..." -f cyan
-			
-		}
-		
-	}
-	
-	catch
-	{
-		
-		$_.Exception
-		
-	}
-	
-}
 
 ####################################################
 
