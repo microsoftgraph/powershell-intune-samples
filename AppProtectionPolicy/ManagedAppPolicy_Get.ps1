f59a64c9c51f6a39d8334d315074880d3de56e01
davefalkus <falkus@hotmail.com>
2018-07-31 20:50:34 +0100

    AuthToken Function Update

--- AppProtectionPolicy/ManagedAppPolicy_Get.ps1
+++ AppProtectionPolicy/ManagedAppPolicy_Get.ps1
@@ -38,14 +38,14 @@ $tenant = $userUpn.Host
 Write-Host "Checking for AzureAD module..."
 
     $AadModule = Get-Module -Name "AzureAD" -ListAvailable
-    
+
     if ($AadModule -eq $null) {
-    
+
         Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
         $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
-    
+
     }
-    
+
     if ($AadModule -eq $null) {
         write-host
         write-host "AzureAD Powershell module not installed..." -f Red
@@ -54,36 +54,36 @@ Write-Host "Checking for AzureAD module..."
         write-host
         exit
     }
-    
+
 # Getting path to ActiveDirectory Assemblies
 # If the module count is greater than 1 find the latest version
 
     if($AadModule.count -gt 1){
-    
+
         $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
-    
+
         $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
-    
+
             # Checking if there are multiple versions of the same module found
-    
+
             if($AadModule.count -gt 1){
-    
+
             $aadModule = $AadModule | select -Unique
-    
+
             }
-    
+
         $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
         $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
-    
+
     }
-    
+
     else {
-    
+
         $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
         $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
-    
+
     }
-    
+
 [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
 
 [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
@@ -97,47 +97,60 @@ $resourceAppIdURI = "https://graph.microsoft.com"
 $authority = "https://login.microsoftonline.com/$Tenant"
 
     try {
-    
+
     $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
-    
+
     # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
     # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
-    
+
     $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
-    
+
     $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
-    
-    $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
-    
+
+    $MethodArguments = [Type[]]@("System.String", "System.String", "System.Uri", "Microsoft.IdentityModel.Clients.ActiveDirectory.PromptBehavior", "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier")
+    $NonAsync = $AuthContext.GetType().GetMethod("AcquireToken", $MethodArguments)
+
+        if ($NonAsync -ne $null){
+
+            $authResult = $authContext.AcquireToken($resourceAppIdURI, $clientId, [Uri]$redirectUri, [Microsoft.IdentityModel.Clients.ActiveDirectory.PromptBehavior]::Auto, $userId)
+        
+        }
+        
+        else {
+
+            $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI, $clientId, [Uri]$redirectUri, $platformParameters, $userId).Result 
+        
+        }
+
         # If the accesstoken is valid then create the authentication header
-    
+
         if($authResult.AccessToken){
-    
+
         # Creating header for Authorization token
-    
+
         $authHeader = @{
             'Content-Type'='application/json'
             'Authorization'="Bearer " + $authResult.AccessToken
             'ExpiresOn'=$authResult.ExpiresOn
             }
-    
+
         return $authHeader
-    
+
         }
-    
+
         else {
-    
+
         Write-Host
         Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
         Write-Host
         break
-    
+
         }
-    
+
     }
 
     catch {
-    
+
     write-host $_.Exception.Message -f Red
     write-host $_.Exception.ItemName -f Red
     write-host
