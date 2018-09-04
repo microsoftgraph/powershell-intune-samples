5808dcfdcef0f1fb73905ef5583d49a97c53f402
davefalkus <falkus@hotmail.com>
2018-09-04 21:43:56 +0100

    Fix for JSON Import

--- DeviceConfiguration/DeviceConfiguration_Import_FromJSON.ps1
+++ DeviceConfiguration/DeviceConfiguration_Import_FromJSON.ps1
@@ -328,7 +328,7 @@ break
 $JSON_Data = gc "$ImportPath"
 
 # Excluding entries that are not required - id,createdDateTime,lastModifiedDateTime,version
-$JSON_Convert = $JSON_Data | ConvertFrom-Json | Select-Object -Property * -ExcludeProperty id,createdDateTime,lastModifiedDateTime,version
+$JSON_Convert = $JSON_Data | ConvertFrom-Json | Select-Object -Property * -ExcludeProperty id,createdDateTime,lastModifiedDateTime,version,supportsScopeTags
 
 $DisplayName = $JSON_Convert.displayName
 
